5998b51cdfbfcbd279beb81efacc25a28863a2cb
RupuSaha <pritam.a.saha@capgemini.com>
2019-07-18 19:25:10 +0530

    test

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ async def run(scrapped_po_no):
     s = requests.Session()
     s.auth = ('pritamsa', 'rupu@0801')
     #loop = asyncio.new_event_loop()
-    loop = asyncio.get_event_loop()
+    #loop = asyncio.get_event_loop()
     future1 = loop.run_in_executor(None, s.get, "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")?sap-client=400&$format=json",s.auth)
     future2 = loop.run_in_executor(None, s.get, "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json",s.auth)
     response1 = await future1
@@ -91,7 +91,7 @@ def query_get_task_with_details(bot_memo,present_skill):
             
             # response_po_item_detail = requests.get("https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json",auth=HTTPBasicAuth('pritamsa', 'rupu@0801'))
             
-            body1,body2 = run(scrapped_po_no)
+            body2,body3 = run(scrapped_po_no)
             loop = asyncio.get_event_loop()
             loop.run_until_complete(run(scrapped_po_no))
             loop.close()

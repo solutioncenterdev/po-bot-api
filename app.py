82051d57acbfbe06792fa4da372e264b8129f851
RupuSaha <pritam.a.saha@capgemini.com>
2019-07-19 14:24:19 +0530

    test

--- app.py
+++ app.py
@@ -69,21 +69,22 @@ def query_get_task_with_details(bot_memo,present_skill):
             # response_po_item_detail = requests.get("https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json",auth=HTTPBasicAuth('pritamsa', 'rupu@0801'))
 
             
-            url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")?sap-client=400&$format=json"
-            url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json"
+            url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder='4500000365')?sap-client=400&$format=json"
+            url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder='4500000365')/to_PurchaseOrderItem?sap-client=400&$format=json"
             urls = [url1,url2]
 
             rs = (grequests.get(u,auth=('pritamsa','rupu@0801'))for u in urls)
-            reque = grequests.imap(rs,size=1)
+            reque = grequests.imap(rs,size=10)
             response_array = []
             for response in reque:
                 print(response)
-                x = response.json
+                x = response.json()
                 response_array.append(x)
                 # print(x)
-            print(response_array)
-            body2 = response_array[0]
-            body3 = response_array[1]
+            #print(response_array)
+            body2 = response_array[0]  #1st url body
+            body3 = response_array[1] #2nd url body
+            #print(body3['d']['results'][0]['Material'])
             # body2 = response_po_detail_header.json()
             # body3 = response_po_item_detail.json()
             #print(r.json())
--- hello.py
+++ hello.py
@@ -8,20 +8,39 @@ import os
 from requests.auth import HTTPBasicAuth
 
 
-url = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json"
+# url = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json"
 
-url_count = 3
+# url_count = 3
 
-pending_requests = []
-for i in range(url_count):
-  pending_requests.append(grequests.get(url,auth=('pritamsa', 'rupu@0801')))
+# pending_requests = []
+# for i in range(url_count):
+#   pending_requests.append(grequests.get(url,auth=('pritamsa', 'rupu@0801')))
 
-all_responses = grequests.imap(pending_requests,size=1)
-# json_body = [response.content for response in all_responses]
-# print(json_body[0])
-for response in all_responses:
-    x = response.json()
+# all_responses = grequests.imap(pending_requests,size=1)
+# # json_body = [response.content for response in all_responses]
+# # print(json_body[0])
+# for response in all_responses:
+#     x = response.json()
    
     
-    break
-print(x)
+#     break
+# print(x)
+
+
+
+url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder='4500000365')?sap-client=400&$format=json"
+url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder='4500000365')/to_PurchaseOrderItem?sap-client=400&$format=json"
+urls = [url1,url2]
+
+rs = (grequests.get(u,auth=('pritamsa','rupu@0801'))for u in urls)
+reque = grequests.imap(rs,size=10)
+response_array = []
+for response in reque:
+    print(response)
+    x = response.json()
+    response_array.append(x)
+    # print(x)
+#print(response_array)
+body2 = response_array[0]
+body3 = response_array[1]
+print(body3['d']['results'][0]['Material'])

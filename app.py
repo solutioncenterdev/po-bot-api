a1cd6373e564adf2b2114fe653e95b8a43fd6bae
RupuSaha <pritam.a.saha@capgemini.com>
2019-07-19 15:40:44 +0530

    test

--- app.py
+++ app.py
@@ -15,7 +15,26 @@ app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 
+def take_action_async(scrapped_po_no):
+    scrapped_po = scrapped_po_no
+    print('*******************************************************')
+    print(scrapped_po)
+    url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po +"'"")?sap-client=400&$format=json"
+    url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json"
+    urls = [url1,url2]
+    rs = (grequests.get(u,auth=('pritamsa','rupu@0801'))for u in urls)
+    reque = grequests.map(rs,size=10)
+    response_array = []
+    for response in reque:
+        print(response)
+        x = response.json()
+        response_array.append(x)
+        # print(x)
+    #print(response_array)
+    body2 = response_array[0]  #1st url body
+    body3 = response_array[1]
 
+    return body2,body3
 
 
 
@@ -36,12 +55,7 @@ def index():
 
     
 
-    
-    
 
-
-
-    #bot_memo_index_prsent_value = data['conversation']['memory']['index']
     reply,index,instanceID = query_get_task_with_details(data['conversation']['memory'],present_skill)
     return jsonify(
         status=200,
@@ -49,39 +63,26 @@ def index():
         'type': 'text',
         'content': reply
         #'content': 'here is your first task \n'+ task_list[0] + '\n' + task_detail_dict[task_list[0]],
-    
+
         }],
         conversation={ 
     'memory': {'index':index,
     'instanceID':instanceID} 
     } 
     )
+    
+    
+
+
+
+    #bot_memo_index_prsent_value = data['conversation']['memory']['index']
+    
 
     
 
    
    
 def query_get_task_with_details(bot_memo,present_skill):
-
-    def take_action_async(scrapped_po_no):
-        scrapped_po = scrapped_po_no
-        print('*******************************************************')
-        print(scrapped_po)
-        url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po +"'"")?sap-client=400&$format=json"
-        url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json"
-        urls = [url1,url2]
-        rs = (grequests.get(u,auth=('pritamsa','rupu@0801'))for u in urls)
-        reque = grequests.map(rs,size=10)
-        response_array = []
-        for response in reque:
-            print(response)
-            x = response.json()
-            response_array.append(x)
-            # print(x)
-        #print(response_array)
-        body2 = response_array[0]  #1st url body
-        body3 = response_array[1]
-        return body2,body3
     
     if ((bot_memo == {} or bot_memo['index']) and present_skill == 'get_task'):
         r = requests.get("https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json", auth=HTTPBasicAuth('pritamsa', 'rupu@0801'))

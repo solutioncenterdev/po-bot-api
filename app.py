5e302cf4010751492f67d493f49f1d2fab0c10e8
RupuSaha <pritam.a.saha@capgemini.com>
2019-07-25 17:34:57 +0530

    test

--- app.py
+++ app.py
@@ -110,6 +110,9 @@ def query_get_task_with_details(bot_memo,present_skill):
                 Material = body3["d"]["results"][i]["Material_Text"]
                 Plant = body3["d"]["results"][i]["Plant"]
                 OrderQuantity = body3["d"]["results"][i]["OrderQuantity"]
+                netPriceItem = body3["d"]["results"][i]["NetPriceAmount"]
+                documentCurrency = body3["d"]["results"][i]["DocumentCurrency"]
+                item_dict = {'Material':Material}
                 
                 concat_string_for_multiple_lineitems = concat_string_for_multiple_lineitems \
                     + 'Material: ' + Material + '.\n' + 'plant: ' + Plant + '.\n' \
@@ -272,7 +275,7 @@ def query_get_task_with_details(bot_memo,present_skill):
 
         response = session.head("https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json", auth=HTTPBasicAuth('pritamsa', 'rupu@0801'),headers=header)
         if (response.status_code != 200):
-            return approval_failure_reply,bot_memo['index'],present_task_instance_id
+            return approval_failure_reply,bot_memo['index'],present_task_instance_id,created_by_user,SupplierName, (PurchaseOrderNetAmount + ' ' + DocumentCurrency)
         elif (response.status_code == 200):
             cookie = session.cookies.get_dict()
             print(cookie)
@@ -288,7 +291,7 @@ def query_get_task_with_details(bot_memo,present_skill):
             print('***************************************************************')
             print(approve_po.status_code)
 
-            return after_approval_reply,bot_memo['index'],present_task_instance_id,created_by_user,SupplierName, (PurchaseOrderNetAmount + ' ' + DocumentCurrency)  #after this call the "next" task showing skill in bot
+            return after_approval_reply + bot_memo['present_reply'],bot_memo['index'],present_task_instance_id,created_by_user,SupplierName, (PurchaseOrderNetAmount + ' ' + DocumentCurrency)  #after this call the "next" task showing skill in bot
 
 
     

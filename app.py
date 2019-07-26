a63a8b1505bba42afba61c2e83b5478f45c3c113
RupuSaha <pritam.a.saha@capgemini.com>
2019-07-26 13:10:49 +0530

    test

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def query_get_task_with_details(bot_memo,present_skill):
             final_reply_string = 'Now you have got, '+ str(no_of_tasks) + ' pending tasks to approve. ' + get_task_string + get_task_string_with_header_detail +'You have: ' + str(no_of_line_items) +' items.\n'+ concat_string_for_multiple_lineitems + " say approve to approve this task or say ignore to skip this task and move on to your next task, or say next to get your next task with details."
             
 
-            return final_reply_string,1,instance_id,created_by_user,SupplierName, (PurchaseOrderNetAmount + ' ' + DocumentCurrency),'' #return 1for memory index as no memo is present in the beggining
+            return bot_memo['after_approval_reply']+ final_reply_string,1,instance_id,created_by_user,SupplierName, (PurchaseOrderNetAmount + ' ' + DocumentCurrency),'' #return 1for memory index as no memo is present in the beggining
 
         else:
             final_reply_string = 'no tasks to approve...'
@@ -270,7 +270,7 @@ def query_get_task_with_details(bot_memo,present_skill):
     #         return final_reply_string,bot_memo['index'],len(body1["d"]["results"]),created_by_user,SupplierName, (PurchaseOrderNetAmount + ' ' + DocumentCurrency)
 
     elif((bot_memo['index']) and present_skill == 'approve'):
-        after_approval_reply = 'successfully approved, say next to get your next task to approve....'
+        after_approval_reply = 'successfully approved'
         approval_failure_reply = "there was an issue with the server, Please try again later to approve..."
         session = requests.Session()
         header = {'x-csrf-token':'Fetch'}

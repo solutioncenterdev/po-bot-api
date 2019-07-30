3237ce9cabe1ddddf4aad2320cb8cf29a896a40c
RupuSaha <pritam.a.saha@capgemini.com>
2019-07-30 16:26:19 +0530

    test

--- app.py
+++ app.py
@@ -384,12 +384,14 @@ def query_get_task_with_details(bot_memo,present_skill,bot_nlp):
             return after_approval_reply,bot_memo['index'],present_task_instance_id,bot_memo['created_by'],bot_memo['SupplierName'], bot_memo['PurchaseOrderNetAmount'],after_approval_reply,''  #after this call the "next" task showing skill in bot
 
     elif((bot_nlp['ordinal']) and present_skill == 'get_item_details'):
-        filter_item_ordinally = 'item : '+ (bot_nlp['ordinal'][bot_nlp['ordinal']['index']]['rank'])
-        print(filter_item_ordinally)
-        print(type(filter_item_ordinally))
-        item_level_reply_ordibnally = bot_memo['all_item_details'][filter_item_ordinally]
+        # filter_item_ordinally = 'item : '+ (bot_nlp['ordinal'][bot_nlp['ordinal']['index']]['rank'])
+        # print(filter_item_ordinally)
         print('///////////////////////////////////////////////////')
-        print(item_level_reply_ordibnally)
+        print(bot_nlp['ordinal']['index'])
+        print(type(bot_nlp['ordinal']['index']))
+        # item_level_reply_ordibnally = bot_memo['all_item_details'][filter_item_ordinally]
+        # print('///////////////////////////////////////////////////')
+        # print(item_level_reply_ordibnally)
         
 
 @app.route('/errors', methods=['POST'])

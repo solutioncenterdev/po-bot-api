8c4230f0be9613853e474ee264d7903f32d1f81d
RupuSaha <pritam.a.saha@capgemini.com>
2019-08-01 17:36:18 +0530

    testnew

--- app.py
+++ app.py
@@ -22,6 +22,44 @@ app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 
+async def fetch(session, url):
+    async with session.get(url) as response:
+        #data = await response.read()
+        
+        return await response.json()
+async def hey():
+
+    r = requests.get("https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json", auth=HTTPBasicAuth('pritamsa', 'rupu@0801'))
+    body1 = r.json()
+    no_of_tasks = len(body1["d"]["results"])
+    if (body1["d"]["results"]):
+        #task details
+        instance_id = body1["d"]["results"][0]["InstanceID"] 
+        task_title = body1["d"]["results"][0]["TaskTitle"]
+        
+        scrapped_po_no = task_title.split("order ",1)[1]
+
+    # else:
+    #     final_reply_string = 'no more tasks to approve in your inbox.'
+    #     return final_reply_string,1,bot_memo,bot_memo,bot_memo, bot_memo,'','',''
+
+        url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")?sap-client=400&$format=json"
+        url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json"
+        urls = [url1,url2]
+
+        # urls = ["https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json", "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po +"'"")?sap-client=400&$format=json"]
+        tasks = []
+        async with aiohttp.ClientSession(auth=aiohttp.BasicAuth('pritamsa','rupu@0801')) as session:
+            for url in urls:
+                tasks.append(fetch(session,url))
+
+            body = await asyncio.gather(*tasks)
+
+            return body[0], body[1], no_of_tasks, instance_id, task_title
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(hey())
 
 
         
@@ -100,19 +138,52 @@ def index():
 def query_get_task_with_details(bot_memo,present_skill,bot_nlp):
     
     if ((bot_memo == {} or bot_memo['index']) and present_skill == 'get_task'):
+
+
+        async def fetch(session, url):
+            async with session.get(url) as response:
+        #data = await response.read()
+        
+                return await response.json()
+        async def hey():
+
+            r = requests.get("https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json", auth=HTTPBasicAuth('pritamsa', 'rupu@0801'))
+            body1 = r.json()
+            no_of_tasks = len(body1["d"]["results"])
+            if (body1["d"]["results"]):
+        #task details
+                instance_id = body1["d"]["results"][0]["InstanceID"] 
+                task_title = body1["d"]["results"][0]["TaskTitle"]
+        
+                scrapped_po_no = task_title.split("order ",1)[1]
+
+    # else:
+    #     final_reply_string = 'no more tasks to approve in your inbox.'
+    #     return final_reply_string,1,bot_memo,bot_memo,bot_memo, bot_memo,'','',''
+
+                url1 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")?sap-client=400&$format=json"
+                url2 = "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/ALEXA_ALL/C_PURCHASEORDER_FS_SRV;o=sid(M17.400)/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po_no +"'"")/to_PurchaseOrderItem?sap-client=400&$format=json"
+                urls = [url1,url2]
+
+        # urls = ["https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/Workflow_approval/TaskCollection?sap-client=400&$filter=Status%20eq%20%27READY%27&$format=json", "https://p2001172697trial-trial.apim1.hanatrial.ondemand.com/p2001172697trial/C_PURCHASEORDER_FS_SRV/C_PurchaseOrderFs(PurchaseOrder="+ "'"+scrapped_po +"'"")?sap-client=400&$format=json"]
+                tasks = []
+                async with aiohttp.ClientSession(auth=aiohttp.BasicAuth('pritamsa','rupu@0801')) as session:
+                    for url in urls:
+                        tasks.append(fetch(session,url))
+
+                        body = await asyncio.gather(*tasks)
+
+                        body2 = body[0]
+                        body3 = body[1]
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(hey())
         
         
         
             
-            body2,body3,no_of_tasks,instance_id,task_title = test.hey()
-
-            # loop = asyncio.get_event_loop()
-            # loop.run_until_complete(hey())
+            # body2,body3,no_of_tasks,instance_id,task_title = test.hey()
 
            
-
-            # loop = asyncio.get_event_loop()
-            # loop.run_until_complete(hey(scrapped_po_no))
             
             #po_header detail
             created_by_user = body2["d"]["CreatedByUser"]

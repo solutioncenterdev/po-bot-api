f5ff9c1c980e90a146ce02d070446d2366eb5e7d
RupuSaha <pritam.a.saha@capgemini.com>
2019-08-24 14:20:42 +0530

    nikolla1

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_taskONEbyONE(bot_memo,present_skill,bot_nlp):
         if (eval(PurchaseOrderNetAmount) <= 7000):
             suggestion_reply = "I recommend you to approve this PO since, you have approved similar orders before. "
         else:
-            suggestion_reply = "I recommend you to get the details of the purchase order before approving."
+            suggestion_reply = "I recommend you to get the details of the purchase order before taking any action."
 
         final_reply_string = ''
         concat_string_for_multiple_lineitems = ''
@@ -228,7 +228,7 @@ def query_get_task_with_details(bot_memo,present_skill,bot_nlp):
                 suggestion_reply = "I recommend you to approve this PO since, you have approved similar orders before."
 
             else:
-                suggestion_reply = ""
+                suggestion_reply = "I recommend you to get the details of the purchase order before taking any action."
 
             final_reply_string = ''
             concat_string_for_multiple_lineitems = ''
@@ -427,7 +427,7 @@ def query_get_task_with_details(bot_memo,present_skill,bot_nlp):
                 suggestion_reply = "I recommend you to approve this PO since, you have approved similar orders before. Anyways, you can always get the item details before taking any action"
 
             else:
-                suggestion_reply = ""
+                suggestion_reply = "I recommend you to get the details of the purchase order before taking any action."
 
             
             final_reply_string = ''
@@ -501,7 +501,7 @@ def query_get_task_with_details(bot_memo,present_skill,bot_nlp):
                 suggestion_reply = "I recommend you to approve this PO since, you have approved similar orders before. Anyways, you can always get the item details before taking any action"
 
             else:
-                suggestion_reply = ""
+                suggestion_reply = "I recommend you to get the details of the purchase order before taking any action."
 
             
             final_reply_string = ''
